9e14b14ba93b1108d847375ddad39a73557fa347
capitalch <capitalch@gmail.com>
2018-02-10 14:20:17 +0530

    init

---
+++ src/app/snapshot/snapshot.module.ts
@@ -0,0 +1,48 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
+import { SnapshotComponent } from './snapshot.component';
+// Patient Concerns component
+import { PatientConcernsModule } from '@sutterhealth/patient-concerns';
+
+import { NeuroGraphModule } from 'neuro-graph';
+// import { NeuroGraphModule } from '../neuro-graph/neuro-graph.module';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+// Progress Note
+import { ProgressNotesModule } from '@sutterhealth/progress-notes';
+import { DragulaModule } from 'ng2-dragula';
+
+import {
+  MdTooltipModule,
+  MdCardModule,
+  MdDialogModule,
+  MatInputModule,
+  MdListModule,
+  MdMenuModule,
+  MdRadioModule,
+  MdRippleModule,
+  MdSelectModule,
+  MdToolbarModule,
+} from '@angular/material';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    SharedModule,
+    MdSelectModule,
+    MdTooltipModule,
+    MdCardModule,
+    MdDialogModule,
+    PatientConcernsModule.forRoot(),
+    MatInputModule,
+    FlexLayoutModule,
+    NeuroGraphModule.forRoot(),
+    ProgressNotesModule,
+    DragulaModule
+  ],
+  declarations: [
+    SnapshotComponent
+  ]
+})
+export class SnapshotModule { }
---
+++ src/assets/options.js
@@ -0,0 +1,43 @@
+var gUrlMaps = {
+
+    "http:get:dmt": "http://dcplrdd101:8989/neuroshare/api/internal/ms/dmt/",
+    "http:post:dmt": "http://dcplrdd101:8989/neuroshare/api/internal/ms/dmt",
+    "http:put:dmt": "http://dcplrdd101:8989/neuroshare/api/internal/ms/dmt",
+
+    "http:get:otherMeds": "http://dcplrdd101:8989/neuroshare/api/internal/ms/other-meds/",
+    "http:post:otherMeds": "http://dcplrdd101:8989/neuroshare/api/internal/ms/other-meds",
+    "http:put:otherMeds": "http://dcplrdd101:8989/neuroshare/api/internal/ms/other-meds",
+
+    "http:get:medications": "http://dcplrdd101:8989/maestro/api/ehr/medications-orders/",
+
+    "http:get:edss": "http://dcplrdd101:8989/neuroshare/api/internal/ms/edss-score/",
+    "http:post:edss": "http://dcplrdd101:8989/neuroshare/api/internal/ms/edss-score",
+    "http:put:edss": "http://dcplrdd101:8989/neuroshare/api/internal/ms/edss-score",
+    "http:get:virtualCaseLoad": "http://dcplrdd101:8989/neuroshare/api/internal/ms/ms-population-data/",
+
+    "http:get:walk25Feet": "http://dcplrdd101:8989/neuroshare/api/internal/ms/25walk/",
+    "http:post:walk25Feet": "http://dcplrdd101:8989/neuroshare/api/internal/ms/25walk",
+    "http:put:walk25Feet": "http://dcplrdd101:8989/neuroshare/api/internal/ms/25walk",
+
+    "http:get:walk25Feet:info": "http://dcplrdd101:8989/neuroshare/api/internal/ms/25walk-info/",
+
+    "http:get:relapse": "http://dcplrdd101:8989/neuroshare/api/internal/ms/relapse/",
+    "http:post:relapse": "http://dcplrdd101:8989/neuroshare/api/internal/ms/relapse",
+    "http:put:relapse": "http://dcplrdd101:8989/neuroshare/api/internal/ms/relapse",
+    "http:delete:relapse": "http://dcplrdd101:8989/neuroshare/api/internal/ms/relapse",
+
+    "http:get:imaging": "http://dcplrdd101:8989/neuroshare/api/internal/ms/imaging-orders/",
+
+    "http:get:all:questionnaire": "https://private-anon-34d1685dcf-neuroshareapis.apiary-mock.com/neuroshare/api/all-questionnaire-responses/",
+
+    "http:get:labs": "http://dcplrdd101:8989/neuroshare/api/internal/ms/lab-orders/",
+
+    "http:get:cds:user:data": "http://dcplrdd101:8989/neuroshare/api/internal/ms/cds/",
+    "http:post:cds:user:data": "http://dcplrdd101:8989/neuroshare/api/internal/ms/cds",
+    "http:put:cds:user:data": "http://dcplrdd101:8989/neuroshare/api/internal/ms/cds",
+    "http:get:cds:info": "http://dcplrdd101:8989/neuroshare/api/internal/ms/cds-info/",
+
+    "http:get:encounters": "http://dcplrdd101:8989/maestro/api/ehr/encounters/",
+
+    "http:get:progressNote": "http://dcplrdd101:8989/maestro/api/ehr/notes/",
+};
